80af003e5e5b59c71586d7eda27d5d3b415e1e69
jisoo <107850055+Powkim@users.noreply.github.com>
2023-03-13 19:04:31 +0900

    230313
    230313

--- lib/screens/httptest.dart
+++ lib/screens/httptest.dart
@@ -7,14 +7,21 @@ class MyWidget extends GetxController  {
 MyWidget();
 final dio = Dio();
 
-void getHttp() async {
-  final response = await dio.get('https://test.kees.tv/api/report',options: Options(headers: {'Authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiaG9zZW9uZyBraW0iLCJpZCI6MSwiZW1haWwiOiJnaHRqZDk1N0B1bmRlcnBpbi5rciIsImhhc2giOiIgICAgICAgICJ9.YTVPz3_D4bQ79idYaucGSCFJCg9OfUnckx7GosvHQk4'}));
-  print(response);
+Future <List<dynamic>> getHttp() async {
+
+  final response = await dio.get('http://10.220.210.107:8080/api/report', 
+  queryParameters:
+   {'type': 'POST', 'status': 'PENDING','idOfType':'250','reportOptionId':'0','registerUserId':'0','page':'0','orderBy':'true'},
+  options: Options(headers: 
+  {'Authorization':
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiaG9zZW9uZyBraW0iLCJpZCI6MSwiZW1haWwiOiJnaHRqZDk1N0B1bmRlcnBpbi5rciIsImhhc2giOiIgICAgICAgICJ9.YTVPz3_D4bQ79idYaucGSCFJCg9OfUnckx7GosvHQk4'}));
+print(response);
+return response.data['result'];
 }
 
 void postHttp() async {
-  final response = await dio.post('https://test.kees.tv/api/report', data: { "type":"POST","idOfType":"250","reportOptionId":3});
-
+  final response = await dio.post('https://test.kees.tv/api/report', data: { "type":"POST","idOfType":"250","reportOptionId":3}); 
+}
 // void deleteHttp() async {
 //   final response = await dio.delete('https://dart.dev');
 //   print(response);
@@ -24,5 +31,4 @@ void postHttp() async {
 //   final response = await dio.put('https://dart.dev',data:{'title':'제목 수정','content':'내용 수정'});
 //   print(response);
 // }
-  
-}}
+}
